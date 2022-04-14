aa3fc4009fc3f95b0f753b6d591f7aab71fed96d
kimlee <kimlee@10.111.68.108>
2022-04-14 16:08:49 +0800

    myjava/demo/primitive

---
+++ myjava-c4/src/apiit/myjava/day1/primitive/App.java
@@ -0,0 +1,46 @@
+package apiit.myjava.day1.primitive;
+
+public class App {
+
+	/**
+	 * PRIMITIVE DATA TYPE (PDT)
+	 */
+	
+	/**
+	 * >NUMERICAL TYPE
+	 * byte(8bit) 
+	 * short(16bit)
+	 * int(32bit)
+	 * long(64bit)
+	 
+	 * float(32bit)
+	 * double(64bit)
+	 
+	 * >CHARACTER TYPE
+	 * char(32bit)
+	 * 
+	 * >BOOLEAN-OUTCOME TYPE
+	 * boolean(1bit)
+	 */
+	
+	public static void main(String[] args) {
+		byte b = 1;
+		short s = 1;
+		int i = 1;
+		long l = 1;
+		
+		//float f1 = 1.1; //invalid due to double primitive
+		float f2 = 1.1F; //casted from double to float primitive
+		double d = 1.1;
+		
+		char c = 'A';
+		
+		boolean bool = true;
+		
+		/**
+		 * invalid identifier using reserved words
+		 */
+		//int for = 2;
+	}
+	
+}
