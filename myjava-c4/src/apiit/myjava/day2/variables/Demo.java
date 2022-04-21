c7cd8a7b4824ce98f3b79540a3016d779b5c3919
kimlee <kimlee@10.111.69.57>
2022-04-21 10:58:07 +0800

    demo/variable

---
+++ myjava-c4/src/apiit/myjava/day2/variables/Demo.java
@@ -0,0 +1,28 @@
+package apiit.myjava.day2.variables;
+
+public class Demo {
+
+	/**
+	 * local variable; instance variable; class variable
+	 */
+	
+}
+
+class Var
+{
+	
+	//instance-var (belongs to object)
+	int i1;
+	int i2, i3, i4;
+	
+	//class-var (belongs to class)
+	static String s1, s2, s3;
+	
+	//local-var (defined inside method)
+	void m1()
+	{
+		//local-var
+		int i1 = 10;
+		
+	}
+}
