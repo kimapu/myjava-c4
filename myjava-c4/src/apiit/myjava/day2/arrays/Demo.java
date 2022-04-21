0f753bda1afbd0be2b45834dfaefe5fd3e8a163a
kimlee <kimlee@10.111.69.57>
2022-04-21 11:04:36 +0800

    demo/array

---
+++ myjava-c4/src/apiit/myjava/day2/arrays/Demo.java
@@ -0,0 +1,28 @@
+package apiit.myjava.day2.arrays;
+
+public class Demo {
+
+	/**
+	 * Java array
+	 */
+
+	void m1()
+	{
+		//10 memory locations
+		int[] arr = new int[10];
+		
+		//initialise
+		arr[0] = 10;
+		arr[1] = 20;
+		
+		//access
+		int e1 = arr[0];
+		int e2 = arr[1];
+		
+		//---
+		//shorthand for declaration+instantiation
+		int[] arr2 = {1,2,3,4,5,6,7,8,9,10};
+		
+	}
+	
+}
