4f07c80e6374f8cbbb7ee9bd39ab0dacaf91401f
kimlee <kimlee@10.111.65.98>
2022-05-09 15:01:45 +0800

    demo/overloading

---
+++ myjava-c4/src/apiit/myjava/day4/overloading/Demo.java
@@ -0,0 +1,37 @@
+package apiit.myjava.day4.overloading;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		
+		new Bob().show();
+
+		new Bob().show("arg");
+		
+		new Bob().show( new String[] {"elem1", "elem2"}  );
+		
+	}
+}
+
+class Person{
+
+	void show(String[] msg)
+	{
+		System.out.println( "--- Person > show(msg[]) ---" );
+	}
+	
+}
+
+class Bob extends Person{
+	
+	void show()
+	{
+		System.out.println( "--- Bob > show() ---" );
+	}
+	
+	//overloading method - show()
+	void show( String arg )
+	{
+		System.out.println( "--- Bob > show(arg) ---" );
+	}
+}
