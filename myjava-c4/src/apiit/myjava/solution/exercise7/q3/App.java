c8db09144940ea355ffa5dc52907e630fd157790
kimlee <kimlee@kimlee>
2022-05-17 10:45:51 +0800

    solution/ex/7/q3/App

---
+++ myjava-c4/src/apiit/myjava/solution/exercise7/q3/App.java
@@ -0,0 +1,16 @@
+package apiit.myjava.solution.exercise7.q3;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		String path2File = "src/apiit/myjava/solution/exercise7/q3/alphanumeric.txt";
+		
+		
+		Filter.printInteger(path2File);
+		
+		System.out.println();
+		
+	}
+	
+}
